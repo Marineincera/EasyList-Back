fd75c73c2e55f4b0686427e71766abddf09c8e30
Marine Incera <marine.incera@gmail.com>
2021-06-26 16:33:37 +0200

    controllers: typegroup controller created

--- src/controllers/typegroup.controller.ts
+++ src/controllers/typegroup.controller.ts
@@ -3,12 +3,7 @@ import { Application, Request, Response } from "express";
 import { TypegroupService } from "../services/typegroup.service";
 import { commonController } from "../core/abstract.controller";
 
-
 /**
- * Ce controller servira de modèle pour construire vos différent controller
- * Le controlle est la partie de l'application qui est en charge de la reception
- * des requetes http.
- *
  * @param app l'application express
  */
 export const TypegroupController = (app: Application) => {
@@ -16,7 +11,6 @@ export const TypegroupController = (app: Application) => {
 
   const typegroupRouter = commonController(typegroupService);
 
-  // Ajoutez les nouvelles routes ici
   typegroupRouter.get("/search/:typegroups", async (req: Request, res: Response) => {
     const tag = req.params.tag;
 
